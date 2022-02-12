ff102e414358aa37b3d9e0b122941747fc61dcc8
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2022-02-13 01:34:29 +0700

    INIT

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # libft
 Exercise at 42 Bangkok
+
+## To get man page in txt
+`man <man_page> | col -b > ft_isalpha.txt`
---
+++ ft_atoi.c
@@ -0,0 +1,29 @@
+int	ft_atoi(const char *str)
+{
+	long	i;
+	long	nbr;
+	int		sign;
+
+	i = 0;
+	nbr = 0;
+	sign = 1;
+
+	while (str[i] != '\0' && (ft_isspace(str[i])))
+		i++;
+	if (str[i] != '\0' && str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	} else if (str[i] == '+')
+		i++;
+	while (str[i] != '\0' && ft_isdigit(str[i]))
+	{
+		nbr = nbr * 10 + (str[i] - '0');
+		i++;
+	}
+	return (sign * nbr);
+
+}
+// From man isspace
+// ``\t''``\n''``\v''``\f''``\r''`` ''
+// (str[i] == ' ' || str[i] == '\t' || str[i] == '\r' || str[i] == '\n' || str[i] == '\f' || str[i] == '\v' || str[i] == '\r'))
